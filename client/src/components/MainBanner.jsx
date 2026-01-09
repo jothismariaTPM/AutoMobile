117378b14922cf49a11e8c5d1a106feb2313142d
Mallikarjuna-Bluebex-Official <mallikarjuna@bluebex.com>
2026-01-09 18:15:41 +0530

    Update MainBanner.jsx

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 import { Link } from 'react-router-dom'
 
+const HERO_IMAGES = [
+  assets.car_main,
+  assets.hero1,
+  assets.hero2,
+  assets.hero3,
+  assets.hero4,
+  assets.hero5,
+  assets.hero6,
+]
+
+
 const MainBanner = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prev) => (prev + 1) % HERO_IMAGES.length)
+    }, 4000)
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
-    <div className="relative ">
-      {/* Banner Image */}
+    <div className="relative w-full h-[60vh] sm:h-[70vh] lg:h-[70vh] overflow-hidden">
+      {/* Background Image */}
       <img
-        src={assets.car_main}
+        src={HERO_IMAGES[currentIndex]}
         alt="banner"
-        className="w-full object-cover hidden md:block"
+        className="absolute inset-0 w-full h-full object-cover transition-opacity duration-700"
       />
 
-      <img
-        src={assets.car_main}
-        alt="banner"
-        className="w-full object-cover h-[60vh]  sm:h-[70vh] lg:h-[80vh] md:hidden"
-      />
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/20" />
 
-      {/* Content Overlay */}
-      <div className="absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center  pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24">
+      {/* Content */}
+     {/* Content Overlay */}
+      <div className="absolute inset-0 flex flex-col items-center text-white md:items-start justify-end md:justify-center  pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24">
         <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15'>
           Where your vehicle gets the care it deserves! 
         </h1>
@@ -31,10 +50,11 @@ const MainBanner = () => {
         </Link>
          <Link to={"/products"} className='group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer'>
          Explore deals
-        <img src={assets.black_arrow_icon} alt="arrow" className='transition group-hover:translate-x-1'/>
+        <img src={assets.white_arrow_icon} alt="arrow" className='transition group-hover:translate-x-1'/>
         </Link>
     </div>
       </div>
+
     </div>
   )
 }
